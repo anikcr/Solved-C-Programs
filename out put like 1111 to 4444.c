7335af38102903ba183802e3141d99e3ee83d523
Azhar Islam Anik <16303066@iubat.edu>
2019-10-06 11:14:52 +0600

    Add files via upload

---
+++ out put like 1111 to 4444.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main()
+{
+    int i,j;
+    for(i=1;i<=3;i++)
+    {
+        for(j=3;j<=6;j++)
+        {
+            printf("\t%d",i);
+        }
+        printf("\n");
+    }
+}
